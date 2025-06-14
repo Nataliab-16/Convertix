f12e93ed06db8c5b89830e1abce48f9d576e3f7b
Nataliab-16 <nataliabrandao688@gmail.com>
2025-06-13 23:47:38 -0300

    feat: add dashboard

--- app-convertix/src/app/page.tsx
+++ app-convertix/src/app/page.tsx
@@ -1,10 +1,162 @@
+"use client"
 import Image from "next/image";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+
+const exportarPDF = () => {
+  const doc = new jsPDF();
+
+  doc.text("Relatório de Conversões", 14, 10);
+
+  autoTable(doc, {
+    head: [["Vendedora", "Conversões", "Tentativas", "Taxa de conversão"]],
+    body: [
+      ["Ingrid Povóa", "76", "114", "58,8%"],
+      ["Nicole Gonçalves", "87", "120", "64,7%"],
+      ["Mariana Carvalho", "79", "98", "89,1%"],
+    ],
+    styles: { fontSize: 10 },
+  });
+
+  doc.save("Relatorio.pdf");
+};
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen">
+      <aside
+        id="default-sidebar"
+        className="w-64 h-screen bg-gray-50 dark:bg-gray-800"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-5 h-5 text-yellow-300 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  className="shrink-0 w-5 h-5 text-yellow-300 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Vendedoras</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  className="w-6 h-6 text-yellow-300 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
+                  />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Sair</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
 
-      
+      <main className="flex-1 p-4">
+        <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Gerar relatório</button>
+        
+
+        <h2 className="m-4 text-lg poppins-bold">Painel de conversão</h2>
+        <div className="overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Vendedora
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Conversões
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Tentativas
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Taxa de conversão
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Ingrid Povóa
+                </th>
+                <td className="px-6 py-4">76</td>
+                <td className="px-6 py-4">114</td>
+                <td className="px-6 py-4">58,8%</td>
+              </tr>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Nicole Gonçalves
+                </th>
+                <td className="px-6 py-4">87</td>
+                <td className="px-6 py-4">120</td>
+                <td className="px-6 py-4">64,7%</td>
+              </tr>
+              <tr className="bg-white dark:bg-gray-800">
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Mariana Carvalho
+                </th>
+                <td className="px-6 py-4">79</td>
+                <td className="px-6 py-4">98</td>
+                <td className="px-6 py-4">89,1%</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <button type="button" onClick={exportarPDF} className="my-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Exportar em PDF</button>
+      </main>
     </div>
   );
 }
