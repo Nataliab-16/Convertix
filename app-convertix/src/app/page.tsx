ef2f8b6cb6216bacbf8c289840aee7c56b6e51d3
Nataliab-16 <nataliabrandao688@gmail.com>
2025-07-08 00:51:36 -0300

    ci: add loader

--- app-convertix/src/app/page.tsx
+++ app-convertix/src/app/page.tsx
@@ -29,12 +29,14 @@ function CalendarIcon() {
 }
 
 export default function Home() {
-  
+
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [sellersData, setSellersData] = useState<{ nome: string; conversoes: number; tentativas: number; taxa: string }[]>([]);
   const chartRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState(false);
+
 
   const exportarPDF = async () => {
     const doc = new jsPDF();
@@ -122,6 +124,8 @@ export default function Home() {
         <div className="mb-6 justify-center flex">
           <button
             onClick={async () => {
+              setLoading(true)
+
               alert(`Relatório gerado de ${startDate ? startDate.toLocaleDateString() : "sem data de início"} até ${endDate ? endDate.toLocaleDateString() : "sem data de fim"}`);
               const formatarParaAPI = (data: Date | null): string => {
                 return data ? data.toISOString().split("T")[0] : "";
@@ -137,7 +141,7 @@ export default function Home() {
                 const resposta = await fetch(url);
                 const dados = await resposta.json();
                 console.log("Dados recebidos:", dados);
-                
+
                 // Agrupar vendas por nome_vendedora
                 const contagemPorVendedora: Record<string, number> = {};
                 dados.vendas.forEach((venda: any) => {
@@ -153,7 +157,7 @@ export default function Home() {
                 };
 
                 const sellers = Object.entries(contagemPorVendedora).map(([nome, conversoes]) => {
-                  const tentativas = tentativasMock[nome] || 30; 
+                  const tentativas = tentativasMock[nome] || 30;
                   const taxa = ((conversoes / tentativas) * 100).toFixed(1) + "%";
                   return { nome, conversoes, tentativas, taxa };
                 });
@@ -161,53 +165,64 @@ export default function Home() {
                 setSellersData(sellers);
               } catch (erro) {
                 console.error("Erro ao buscar relatório:", erro);
+              } finally {
+                setLoading(false); // Finaliza loader
               }
             }}
             type="button"
             className=" w-full sm:w-auto text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5"
           >
             Ver relatório do período
-          </button>
+          </button> 
+          
           <button className="w-full mx-4 sm:w-auto text-yellow-700 hover:text-white border border-yellow-800 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5">
             Gerar relatório do dia
           </button>
+
+          {loading ? (<button disabled type="button" className="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+          <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+          </svg>
+          Carregando...
+          </button>) : ""}
         </div>
 
-      {sellersData.length > 0 && (
-        <div>
-          <h2 className="text-lg font-bold mb-4">Painel de conversão</h2>
-          <div className="overflow-x-auto mb-8">
-            <table className="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-4 py-3">Vendedora</th>
-                  <th scope="col" className="px-4 py-3">Conversões</th>
-                  <th scope="col" className="px-4 py-3">Tentativas</th>
-                  <th scope="col" className="px-4 py-3">Taxa de conversão</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sellersData.map(({ nome, conversoes, tentativas, taxa }) => (
-                  <tr
-                    key={nome}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                  >
-                    <th
-                      scope="row"
-                      className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      {nome}
-                    </th>
-                    <td className="px-4 py-4">{conversoes}</td>
-                    <td className="px-4 py-4">{tentativas}</td>
-                    <td className="px-4 py-4">{taxa}</td>
+        {sellersData.length > 0 && (
+          <div>
+            <h2 className="text-lg font-bold mb-4">Painel de conversão</h2>
+            <div className="overflow-x-auto mb-8">
+              <table className="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-4 py-3">Vendedora</th>
+                    <th scope="col" className="px-4 py-3">Conversões</th>
+                    <th scope="col" className="px-4 py-3">Tentativas</th>
+                    <th scope="col" className="px-4 py-3">Taxa de conversão</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {sellersData.map(({ nome, conversoes, tentativas, taxa }) => (
+                    <tr
+                      key={nome}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    >
+                      <th
+                        scope="row"
+                        className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {nome}
+                      </th>
+                      <td className="px-4 py-4">{conversoes}</td>
+                      <td className="px-4 py-4">{tentativas}</td>
+                      <td className="px-4 py-4">{taxa}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      )}
+        )}
         <div className="mb-4">
           <button
             type="button"
@@ -217,7 +232,7 @@ export default function Home() {
             Exportar em PDF
           </button>
         </div>
-        
+
       </main>
 
     </div>
